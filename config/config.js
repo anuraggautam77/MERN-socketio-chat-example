7b6de841a2558b1e1dcd8b6d790da6993089bb96
Anurag <Anuraggauutam77@gmail.com>
2018-02-15 23:41:01 +0530

    dist folder

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://username:password@url:port/db',
 
   db_dev:'mongodb://mystuffuser:mystuffuser123@ds225038.mlab.com:25038/mystuff'
 };
