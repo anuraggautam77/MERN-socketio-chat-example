ee45cd94b63c0e3cf9b99604bdc83f66cd6fa760
Anurag <Anuraggauutam77@gmail.com>
2018-11-06 10:07:01 +0530

    Changes in Db

--- config/configuration.js
+++ config/configuration.js
@@ -1,7 +1,5 @@
 
 module.exports = {
-db: 'mongodb://mystuffuser:mystuffuser123@ds225038.mlab.com:25038/mystuff',
-//db_dev: 'mongodb://localhost:27017/mystuff'
-db_dev: 'mongodb://mystuffuser:mystuffuser123@ds225038.mlab.com:25038/mystuff'
+ db: 'mongodb://localhost:27017/mystuff'
 
 };
